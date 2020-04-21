6dd088476761557255f1f99133ead9f3b77a484b
Adriano <adriano.selaviles@gmail.com>
2020-04-20 17:52:52 -0700

    changes

--- store/db.go
+++ store/db.go
@@ -4,41 +4,40 @@ import "net/http"
 
 //Datastore will allow us to implement a database in multiple ways
 type Datastore interface {
-	//addGroup will add a new group to the database
+	// AddGroup will add a new group to the database
 	AddGroup(*Group) error
 
-	//deleteGroup will remove a group given its group id
+	// DeleteGroup will remove a group given its group id
 	DeleteGroup(string) error
 
-	//addUserToGroup will add a user of a given id to a group of given id
+	// AddUserToGroup will add a user of a given id to a group of given id
 	AddUserToGroup(string, string, string) error
 
-	//removeUserFromGroup will remove a user of a given id from a group of given id
+	// RemoveUserFromGroup will remove a user of a given id from a group of given id
 	RemoveUserFromGroup(string, string, string) error
 
-	//addUser will add a new user to the database given username, password, and email, returns the ID
+	// AddUser will add a new user to the database given username, password, and email, returns the ID
 	AddUser(string, string, string) (string, error)
 
-	//To get a user ID given its username
+	// GetUserID gets a user's ID given their username
 	GetUserID(string) (string, error)
 
-	//deleteUser will remove a user from the database
+	// DeleteUser will remove a user from the database
 	DeleteUser(string) error
 
-	//HTTP Endpoint Handler for listing groups
+	// HTTP Endpoint Handler for listing groups
 	ListGroupsHandler(http.ResponseWriter, *http.Request)
 
-	//HTTP Endpoint Handler for showing a group
+	// HTTP Endpoint Handler for showing a group
 	ShowGroupHandler(http.ResponseWriter, *http.Request)
 
 	PassedBasicAuth(string, string) bool
-
 	GetUserMemberGroups(string) []string
 }
 
 type Group struct {
-	Name        string   `json:"name"`
 	ID          string   `json:"id"`
+	Name        string   `json:"name"`
 	Description string   `json:"description,omitempty"`
 	Members     []string `json:"members,omitempty"` //list of member user uuids
 	Owners      []string `json:"owners,omitempty"`  //list of owner user uuids
